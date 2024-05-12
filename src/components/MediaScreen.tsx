ad5cd201a425bf56eb8388af6e80a03cb43ca1dd
ayushgadre <ayushgadre786@gmail.com0>
2024-05-12 19:37:23 +0530

    Updates minors

--- src/components/MediaScreen.tsx
+++ src/components/MediaScreen.tsx
@@ -12,9 +12,9 @@ export default function MediaScreen() {
   return (
     <div className='homescreen'>
       <div className='flex flex-col items-center m-8'>
-        <p className="md:text-2xl text-lg font-bold mb-4 text-pocket-red">Pocket-FM-Assignment</p>
-        <p className="md:text-2xl text-lg">Audio/Video Player</p>
-        <p className="md:text-lg text-sm">Upload a file or select from sample media</p>
+        <p className="md:text-2xl text-lg font-bold mb-4 text-pocket-AliceBlue">Pocket FM</p>
+        <p className="md:text-2xl text-lg">Audio-Video Player</p>
+        <p className="md:text-lg text-sm">Upload a File or Choose from Sample Media</p>
       </div>
       <div className="flex flex-col lg:flex-row mt-8 md:m-4">
         <div className="w-full lg:w-2/3 mb-4 lg:mb-0 lg:pr-4">
@@ -22,8 +22,8 @@ export default function MediaScreen() {
           {selectedFile ? (
             <MediaPlayer fileUrl={selectedFile} fileType={fileType} thumbnail={thumbnail} />
           ) : (
-            <div className="bg-red-200 h-64 flex items-center justify-center">
-              <p className="text-pocket-red text-lg">Video will be displayed here</p>
+            <div className="bg-AliceBlue-200 h-64 flex items-center justify-center">
+              <p className="text-pocket-AliceBlue text-lg">Video will be displayed here</p>
             </div>
           )}
           <UploadMedia setSelectedFile={setSelectedFile} setFileType={setFileType} setThumbnail={setThumbnail} />
