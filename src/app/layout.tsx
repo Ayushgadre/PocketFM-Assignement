f6e70ef0f729126c3f59389c3b29b630542c3589
ayushgadre <ayushgadre786@gmail.com0>
2024-05-12 13:27:28 +0530

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Media Player",
-  description: "Play your audio/video files with custom player",
+  title: "Pocket FM Media Player",
+  description: "Play your Audio/Video files with custom player",
 };
 
 export default function RootLayout({
